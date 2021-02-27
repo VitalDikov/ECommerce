bbae1562f84e50de0146f57463c49bece9125e52
VitalDikov <vitaldikov@gmail.com>
2021-02-27 14:21:10 +0300

    add fields

--- Models/Entities/Item.cs
+++ Models/Entities/Item.cs
@@ -8,7 +8,7 @@ namespace ECommerce.Models.Entities
     public class Item
     {
 
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
         public string ShortDescription { get; set; }
